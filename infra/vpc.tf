2cf5fc95f93d44a35918f37d67f71d34ae7559a4
sharqX <shariquerahman24@outlook.com>
2024-06-08 14:20:43 +0530

    vpc added resources

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -64,8 +64,38 @@ resource "aws_security_group" "node-sg" {
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
-    }
+    } 
+}
 
+resource "aws_internet_gateway" "ig" {
+  vpc_id = aws_vpc.node-vpc.id
+}
 
-  
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.node-vpc.id
+
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+
+  route  {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
+}
+
+resource "aws_route_table_association" "route1" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.s1.id
+}
+
+resource "aws_route_table_association" "route2" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.s2.id
+}
+
+resource "aws_route_table_association" "route3" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.s3.id
 }
