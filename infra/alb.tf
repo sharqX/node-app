30fc4c41e7f5e15baa68f82b325952a5631c4dc8
sharqX <shariquerahman24@outlook.com>
2024-06-09 20:27:39 +0530

    added alb.tf

---
+++ infra/alb.tf
@@ -0,0 +1,52 @@
+resource "aws_alb" "main" {
+  name            = "cb-load-balancer"
+  subnets         = [aws_subnet.s1.id, aws_subnet.s2.id,aws_subnet.s3.id]
+  security_groups = [aws_security_group.lb.id]
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "cb-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.node-vpc.id
+  target_type = "ip"
+
+}
+
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.id
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.app.id
+    type             = "forward"
+  }
+}
+
+resource "aws_security_group" "lb" {
+  name        = "cb-load-balancer-security-group"
+  description = "controls access to the ALB"
+  vpc_id      = aws_vpc.node-vpc.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol = "tcp"
+    from_port = 443
+    to_port = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
